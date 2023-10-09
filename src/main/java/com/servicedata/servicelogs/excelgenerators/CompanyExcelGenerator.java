525305f17da386ce1366163167fb1dc0fb743eff
Marek Utnik <Marek Utnik@host.docker.internal>
2023-10-09 20:04:26 +0200

    Filled addCompany method

--- src/main/java/com/servicedata/servicelogs/excelgenerators/CompanyExcelGenerator.java
+++ src/main/java/com/servicedata/servicelogs/excelgenerators/CompanyExcelGenerator.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.SortedMap;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.servicedata.servicelogs.models.Company;
@@ -18,27 +21,38 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CompanyExcelGenerator {
 	
     private XSSFWorkbook workbook;
+    private XSSFSheet sheet;
+    private String name;
     private Company company;
     private SortedMap<Machine, ArrayList<ConservationLog>> logs;
+    private int rowCount;
 	
-	public CompanyExcelGenerator(Company company, SortedMap<Machine, ArrayList<ConservationLog>> logs) {
+	public CompanyExcelGenerator(String name, Company company, SortedMap<Machine, ArrayList<ConservationLog>> logs) {
 
 		workbook = new XSSFWorkbook();
 		this.company = company;
 		this.logs = logs;
+		this.name = name;
+		this.rowCount = 0;
 	}
 	
 	private void fillWorkbook() {
-		addCompany(company);
+        sheet = workbook.createSheet("ServiceLogs");
+		addCompany();
 		for (Machine machine : logs.keySet()) {
 			addMachine(machine);
 			addConservationLogDescription();
 			for (ConservationLog log : logs.get(machine)) {
-				addConservationLogDescription();
+				addConservationLog();
 			}	
 		}
 	}
 	
+	private void addConservationLog() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	private void addConservationLogDescription() {
 		// TODO Auto-generated method stub
 		
@@ -49,9 +63,31 @@ public class CompanyExcelGenerator {
 		
 	}
 
-	private void addCompany(Company company) {
-		
+	private void addCompany() {
+        Row row = sheet.createRow(rowCount++);
+        createCell(row, 0, "ID");
+        createCell(row, 1, company.getCompanyId());
+        row = sheet.createRow(rowCount++);
+        createCell(row, 0, "Company Name");
+        createCell(row, 1, company.getCompanyName());
+        row = sheet.createRow(rowCount++);
+        createCell(row, 0, "Company Address");
+        createCell(row, 1, company.getCompanyAddress());
 	}
+	
+    private void createCell(Row row, int columnCount, Object valueOfCell) {
+        sheet.autoSizeColumn(columnCount);
+        Cell cell = row.createCell(columnCount);
+        if (valueOfCell instanceof Integer) {
+            cell.setCellValue((Integer) valueOfCell);
+        } else if (valueOfCell instanceof Long) {
+            cell.setCellValue((Long) valueOfCell);
+        } else if (valueOfCell instanceof String) {
+            cell.setCellValue((String) valueOfCell);
+        } else {
+            cell.setCellValue((Boolean) valueOfCell);
+        }
+    }
 	
     public void generateExcelFile(HttpServletResponse response) throws IOException {
         fillWorkbook();
